fd23d496a754b0a3d78cf48227c2165963933c9b
1107719008 <48972788+1107719008@users.noreply.github.com>
2023-03-13 01:41:07 +0800

    set up collider and spinning function

--- Assets/Scripts/SpinWithMouse.cs
+++ Assets/Scripts/SpinWithMouse.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class SpinWithMouse : MonoBehaviour
 {
-    [SerializeField] public bool a = false;
+    [SerializeField] public bool fixedaxis = true;
     float rotationSpeed = 4f;
+    
     void OnMouseDrag()
     {
+        
         float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
         float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
-        if (a == true)
+        if (fixedaxis == true)
         {
-           transform.Rotate(Vector3.right, YaxisRotation);
+           transform.Rotate(Vector3.right, -YaxisRotation);
         }
          transform.Rotate(Vector3.down, XaxisRotation);
 
